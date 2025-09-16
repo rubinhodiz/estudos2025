0d8d77e6cfac5ec5623c1217447e9a78304c4563
Robson Cesar <rubinhodiz@gmail.com>
2025-09-16 20:20:16 -0300

    CRUD Scaffolding

---
+++ SalesWebMVC/Data/SalesWEBMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWEBMVC.Models;
+
+namespace SalesWEBMVC.Data
+{
+    public class SalesWEBMVCContext : DbContext
+    {
+        public SalesWEBMVCContext (DbContextOptions<SalesWEBMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWEBMVC.Models.Department> Department { get; set; } = default!;
+    }
+}
--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWEBMVC.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SalesWEBMVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWEBMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWEBMVCContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
