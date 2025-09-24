a6125a40c8a90900d56f42f2f902c3af79925363
Robson Cesar <rubinhodiz@gmail.com>
2025-09-23 22:13:16 -0300

    Simple Create form

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWEBMVC.Models;
 using SalesWEBMVC.Services;
 
 
@@ -19,5 +20,19 @@ namespace SalesWEBMVC.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return  RedirectToAction(nameof(Index));
+        }
+
     }
 }
